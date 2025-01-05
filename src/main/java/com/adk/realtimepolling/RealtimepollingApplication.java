706f31019e877a3ac5e049c1f948fcd25350d236
Austin Keiber <adkeiber@gmail.com>
2025-01-04 21:02:51 -0500

    init commit

---
+++ src/main/java/com/adk/realtimepolling/RealtimepollingApplication.java
@@ -0,0 +1,13 @@
+package com.adk.realtimepolling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimepollingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimepollingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adk/realtimepolling/RealtimepollingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adk.realtimepolling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimepollingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
