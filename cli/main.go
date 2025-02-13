6008e28456b734588fcc870fc6975c2d5ac81a91
Austin Keiber <adkeiber@gmail.com>
2025-02-12 20:10:22 -0500

    cobra project

--- cli/main.go
+++ cli/main.go
@@ -1,21 +1,11 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
- "flag"
- "fmt"
-)
-
-var name string
-
-func init() {
- flag.StringVar(&name, "name", "", "Name to greet")
- flag.Parse()
-}
+import "polling/cmd"
 
 func main() {
- if name == "" {
-  fmt.Println("Please provide a name using the -name flag")
-  return
- }
- fmt.Printf("Hello, %s!\n", name)
+	cmd.Execute()
 }
--- cli/vote/go.mod
+++
@@ -1,3 +0,0 @@
-module adkeiber/vote
-
-go 1.23.6
